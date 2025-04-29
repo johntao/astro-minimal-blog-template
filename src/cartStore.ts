866de34b57a935c583a5af049043988e8bfa2061
John Tao <80338582+johntao@users.noreply.github.com>
2025-04-29 16:59:16 +0800

    feat: implement persistent cart state and add dummy page

--- src/cartStore.ts
+++ src/cartStore.ts
@@ -1,6 +1,10 @@
-import { atom, map } from 'nanostores';
+// import { atom, map } from 'nanostores';
+import { persistentAtom, persistentMap } from '@nanostores/persistent';
 
-export const isCartOpen = atom(false);
+export const isCartOpen = persistentAtom<boolean>('cart.open:', false, {
+	encode: JSON.stringify,
+	decode: JSON.parse,
+});
 
 export type CartItem = {
 	id: string;
@@ -11,7 +15,10 @@ export type CartItem = {
 
 export type CartItemDisplayInfo = Pick<CartItem, 'id' | 'name' | 'imageSrc'>;
 
-export const cartItems = map<Record<string, CartItem>>({});
+export const cartItems = persistentMap<Record<string, CartItem>>('cart.items:', {}, {
+	encode: JSON.stringify,
+	decode: JSON.parse,
+});
 
 export function addCartItem({ id, name, imageSrc }: CartItemDisplayInfo) {
 	const existingEntry = cartItems.get()[id];
