287430a693abd2aca9cf54963121d6f0264a511d
John Tao <80338582+johntao@users.noreply.github.com>
2025-04-30 16:11:23 +0800

    [refactor]: rewrite styling with TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0070f3',
+        secondary: '#005bb5',
+      },
+      maxWidth: {
+        'container': '800px',
+      },
+    },
+  },
+  plugins: [],
+}
