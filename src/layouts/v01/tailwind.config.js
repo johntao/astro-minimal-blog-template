697e31e423c1583c90cdc2f057b900888d778d01
John Tao <80338582+johntao@users.noreply.github.com>
2025-05-13 23:02:58 +0800

    [style]: implement RWD
    - make giscus click and load
    - rework article heading prefix

--- src/layouts/v01/tailwind.config.js
+++ src/layouts/v01/tailwind.config.js
@@ -9,8 +9,8 @@ const em = (px, base) => `${round(px / base)}em`
 
 const uniHeading = {
   fontSize: em(24, 18),
-  marginTop: em(40, 24),
-  marginBottom: em(16, 24),
+  marginTop: em(30, 24),
+  marginBottom: em(12, 24),
   lineHeight: round(36 / 24),
 };
 
@@ -27,15 +27,15 @@ module.exports = {
           ],
         },
         //fontSize values copy from lg
-        //find a way to import default configs
         sm: {
           css: [
             {
               fontSize: rem(16), //from base
               // fontSize: rem(18), //from lg
-              // lineHeight: round(32 / 18),
               h1: {
-                fontSize: em(48, 18),
+                fontSize: em(36, 18),
+                marginBottom: em(12, 30),
+                lineHeight: round(24 / 30),
               },
               h2: uniHeading,
               h3: uniHeading,
@@ -51,10 +51,144 @@ module.exports = {
               },
               pre: {
                 fontSize: em(16, 18),
-                // lineHeight: round(28 / 16),
               },
               table: {
                 fontSize: em(16, 18),
+              },
+            },
+          ],
+        },
+        lg: {
+          css: [
+            {
+              lineHeight: round(24 / 14),
+              p: {
+                marginTop: em(16, 14),
+                marginBottom: em(16, 14),
+              },
+              '[class~="lead"]': {
+                lineHeight: round(28 / 18),
+                marginTop: em(16, 18),
+                marginBottom: em(16, 18),
+              },
+              blockquote: {
+                marginTop: em(24, 18),
+                marginBottom: em(24, 18),
+                paddingInlineStart: em(20, 18),
+              },
+              h1: {
+                marginTop: '0',
+                marginBottom: em(24, 30),
+                lineHeight: round(36 / 30),
+              },
+              h2: uniHeading,
+              h3: uniHeading,
+              h4: uniHeading,
+              img: {
+                marginTop: em(24, 14),
+                marginBottom: em(24, 14),
+              },
+              picture: {
+                marginTop: em(24, 14),
+                marginBottom: em(24, 14),
+              },
+              video: {
+                marginTop: em(24, 14),
+                marginBottom: em(24, 14),
+              },
+              kbd: {
+                paddingTop: em(2, 14),
+                paddingInlineEnd: em(5, 14),
+                paddingBottom: em(2, 14),
+                paddingInlineStart: em(5, 14),
+              },
+              pre: {
+                lineHeight: round(20 / 12),
+                marginTop: em(20, 12),
+                marginBottom: em(20, 12),
+                borderRadius: rem(4),
+                paddingTop: em(8, 12),
+                paddingInlineEnd: em(12, 12),
+                paddingBottom: em(8, 12),
+                paddingInlineStart: em(12, 12),
+              },
+              ol: {
+                marginTop: em(16, 14),
+                marginBottom: em(16, 14),
+                paddingInlineStart: em(22, 14),
+              },
+              ul: {
+                marginTop: em(16, 14),
+                marginBottom: em(16, 14),
+                paddingInlineStart: em(22, 14),
+              },
+              li: {
+                marginTop: em(4, 14),
+                marginBottom: em(4, 14),
+              },
+              'ol > li': {
+                paddingInlineStart: em(6, 14),
+              },
+              'ul > li': {
+                paddingInlineStart: em(6, 14),
+              },
+              '> ul > li p': {
+                marginTop: em(8, 14),
+                marginBottom: em(8, 14),
+              },
+              '> ul > li > p:first-child': {
+                marginTop: em(16, 14),
+              },
+              '> ul > li > p:last-child': {
+                marginBottom: em(16, 14),
+              },
+              '> ol > li > p:first-child': {
+                marginTop: em(16, 14),
+              },
+              '> ol > li > p:last-child': {
+                marginBottom: em(16, 14),
+              },
+              'ul ul, ul ol, ol ul, ol ol': {
+                marginTop: em(8, 14),
+                marginBottom: em(8, 14),
+              },
+              dl: {
+                marginTop: em(16, 14),
+                marginBottom: em(16, 14),
+              },
+              dt: {
+                marginTop: em(16, 14),
+              },
+              dd: {
+                marginTop: em(4, 14),
+                paddingInlineStart: em(22, 14),
+              },
+              hr: {
+                marginTop: em(40, 14),
+                marginBottom: em(40, 14),
+              },
+              table: {
+                lineHeight: round(18 / 12),
+              },
+              'thead th': {
+                paddingInlineEnd: em(12, 12),
+                paddingBottom: em(8, 12),
+                paddingInlineStart: em(12, 12),
+              },
+              'tbody td, tfoot td': {
+                paddingTop: em(8, 12),
+                paddingInlineEnd: em(12, 12),
+                paddingBottom: em(8, 12),
+                paddingInlineStart: em(12, 12),
+              },
+              figure: {
+                marginTop: em(24, 14),
+                marginBottom: em(24, 14),
+              },
+              figcaption: {
+                fontSize: em(12, 14),
+                lineHeight: round(16 / 12),
+                marginTop: em(8, 12),
               },
             },
           ],
