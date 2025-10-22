6a2729399f758487e11f7c4cc8061289d0d8de85
John Tao <zytao24@gmail.com>
2025-10-22 17:57:59 +0800

    devops: deploy to GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://johntao.github.io',
+    base: '/astro-minimal-blog-template',
+    build: {
+        format: 'file',
+    },
+});
